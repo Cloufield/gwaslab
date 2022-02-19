6d5fb18cf85289dc869525fca94c3a2b4be47da5
Cloufied <610935659@qq.com>
2022-02-19 13:45:26 +0900

    add h2 conversion

--- src/gwaslab/__init__.py
+++ src/gwaslab/__init__.py
@@ -3,3 +3,4 @@ from gwaslab.manhattan import mplot
 from gwaslab.qqplot import qqplot
 from gwaslab.qqplot import gc
 from gwaslab.get_hapmap3 import ldsc
+from gwaslab.h2_conversion import h2_obs_to_liab
