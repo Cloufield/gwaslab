6b3ca59ecb56a547c8d60d65a22bdeb1d9c67638
Cloufied <610935659@qq.com>
2022-11-12 01:06:53 +0900

    fixed bugs

--- src/gwaslab/to_formats.py
+++ src/gwaslab/to_formats.py
@@ -38,7 +38,7 @@ def tofmt(sumstats,
     
     if verbose: log.write(" - Start outputting sumstats in "+fmt+" format...")
     if xymt_number is False:
-        sumstats["CHR"]= sumstats["CHR"].map(get_number_to_chr(xymt=xymt),prefix=chr_prefix)
+        sumstats["CHR"]= sumstats["CHR"].map(get_number_to_chr(xymt=xymt,prefix=chr_prefix))
     elif chr_prefix is not None:
         sumstats["CHR"]= chr_prefix + sumstats["CHR"].astype("string")
     
