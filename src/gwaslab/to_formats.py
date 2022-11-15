d27b61b276e525da2a71178b80f4905000f5aeb6
Cloufield <610935659@qq.com>
2022-11-15 18:05:25 +0900

    fixed bugs

--- src/gwaslab/to_formats.py
+++ src/gwaslab/to_formats.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import hashlib
 from gwaslab.Log import Log
 from gwaslab.CommonData import get_format_dict
 from gwaslab.CommonData import get_number_to_chr
@@ -6,17 +7,15 @@ from gwaslab.CommonData import gwaslab_info
 from pysam import tabix_compress 
 from pysam import tabix_index
 from gwaslab.CommonData import get_formats_list
-import hashlib
 
 # to vcf
 # to fmt
-    ## general : ldsc, plink, plink2, saige, regenie ...
+    ## vcf
     ## vep
     ## bed
     ## annovar
-
+    ## general : ldsc, plink, plink2, saige, regenie 
 ###################################################################################################################################################
-
     
 ###################################################################################################################################################
 def tofmt(sumstats,
@@ -36,6 +35,8 @@ def tofmt(sumstats,
           log=Log(),
           to_csvargs={}):
     
+    if fmt in ["ssf"]: 
+        xymt_number=True
     if verbose: log.write(" - Start outputting sumstats in "+fmt+" format...")
     if xymt_number is False and sumstats["CHR"].dtype in ["Int64","int"]:
         sumstats["CHR"]= sumstats["CHR"].map(get_number_to_chr(xymt=xymt,prefix=chr_prefix))
@@ -211,16 +212,20 @@ def tofmt(sumstats,
     elif fmt=="vcf":
         if verbose: log.write(" -"+fmt+" format will be loaded...")
         meta_data,rename_dictionary = get_format_dict(fmt,inverse=True)
+        #loading format data
         if verbose:             
             log.write(" -"+fmt+" format meta info:")   
             for key,value in meta_data.items():
                 if key not in ["format_fixed_header","format_contig_19","format_contig_38"]:
                     log.write("  -",key," : ",value)
+        
+        # determine which ID to use
         if "rsID" in sumstats.columns:
             rename_dictionary["rsID"]="ID"
         else:
-            rename_dictionary["SNPID"]="ID"
-        
+            rename_dictionary["SNPID"]="ID" 
+
+        # logging
         if verbose: 
             log.write(" -gwaslab to "+fmt+" format dictionary:")  
             keys=[]
@@ -228,36 +233,36 @@ def tofmt(sumstats,
             for key,value in rename_dictionary.items():
                 keys.append(key)
                 values.append(value)
-            log.write("  - gwaslab keys:",keys) 
-            log.write("  - "+fmt+" values:",values) 
-            
+            log.write("  - gwaslab keys:",','.join(keys)) 
+            log.write("  - "+fmt+" values:",','.join(values)) 
+        
+        # get the columns to output
         ouput_cols=[]
         for i in sumstats.columns:
             if i in rename_dictionary.keys():
                 ouput_cols.append(i)  
-        
         ouput_cols = ouput_cols +["STATUS"]+ cols
         sumstats = sumstats.loc[:,ouput_cols]
         sumstats = sumstats.rename(columns=rename_dictionary) 
         
+        # calculate meta data
         harmonised = sum(sumstats["STATUS"].str.match( r"\w\w\w[0][0123][012][01234]", case=False, flags=0, na=False ) )
         switchedalleles = sum(sumstats["STATUS"].str.match( r"\w\w\w[0][0123][12][24]", case=False, flags=0, na=False ) )
         sumstats["ID"] = sumstats["ID"].str.replace(":","_")
         
+        # process Allele frequency data
         if "AF" in sumstats.columns:
             sumstats["INFO"] = "AF="+sumstats["AF"].astype("string")
         else:
             sumstats["INFO"] = "."
-            
+
+        # sumstats columns placed in vcf-FORMAT column     
         output_format=[]
         for i in sumstats.columns:
             if i in meta_data["format_format"]:
                 output_format.append(i)  
-                
-        path = path + "."+suffix
-        if verbose: log.write(" -vcf header contig build:"+str(build)) 
+
         # Create vcf header
-        
         vcf_header= meta_data["format_fixed_header"] +"\n"+ meta_data["format_contig_"+str(build)]+"\n"
         # Create sample header
         vcf_header+="##SAMPLE=<ID={},TotalVariants={},VariantsNotRead=0,HarmonisedVariants={},VariantsNotHarmonised={},SwitchedAlleles={},StudyType={}>\n".format(meta["Name"],len(sumstats),harmonised,len(sumstats)-harmonised,switchedalleles,meta["StudyType"])
@@ -270,6 +275,12 @@ def tofmt(sumstats,
         #VariantsNotHarmonised = 
         #StudyType=
         ##SAMPLE=<ID=IEU-b-1,TotalVariants=9851866,VariantsNotRead=0,HarmonisedVariants=9851866,VariantsNotHarmonised=0,SwitchedAlleles=9851866,StudyType=Continuous>
+        
+        
+        # determine path
+        path = path + "."+suffix
+        if verbose: log.write(" -Output path:",path) 
+        if verbose: log.write(" -vcf header contig build:"+str(build)) 
         
         # output header
         with open(path,"w") as file:
@@ -292,8 +303,7 @@ def tofmt(sumstats,
                 FORMAT=":".join(output_format)
                 DATA=":".join(row[output_format].astype("string"))
                 file.write(CHROM+"\t"+POS+"\t"+ID+"\t"+REF+"\t"+ALT+"\t"+QUAL+"\t"+FILTER+"\t"+INFO+"\t"+FORMAT+"\t"+DATA+"\n")
-        if verbose: log.write(" -Output path:",path) 
-#        if path is not None: sumstats.to_csv(path,sep="\t",index=None,mode='a',**to_csvargs)
+       
         if bgzip is True:
             if verbose: log.write(" -bgzip compressing ...") 
             tabix_compress(path, path+".gz",force=True)
@@ -316,8 +326,8 @@ def tofmt(sumstats,
             for key,value in rename_dictionary.items():
                 keys.append(key)
                 values.append(value)
-            log.write("  - gwaslab keys:",keys) 
-            log.write("  - "+fmt+" values:",values) 
+            log.write("  - gwaslab keys:",  ','.join(keys)) 
+            log.write("  - "+fmt+" values:"  , ','.join(values))
         
         # grab format cols that exist in sumstats
         ouput_cols=[]
@@ -330,7 +340,7 @@ def tofmt(sumstats,
         sumstats = sumstats.loc[:,ouput_cols]
         sumstats = sumstats.rename(columns=rename_dictionary) 
         path = path + "."+suffix+".tsv.gz"
-        if verbose: log.write(" -Output columns:",sumstats.columns)
+        if verbose: log.write(" -Output columns:",','.join(list(sumstats.columns)))
         if verbose: log.write(" -Output path:",path) 
         if path is not None: 
             sumstats.to_csv(path,sep="\t",index=None,**to_csvargs)
@@ -338,7 +348,7 @@ def tofmt(sumstats,
         return sumstats  
     
 def md5sum_file(filename,log,verbose):
-    if verbose: log.write(" -md5sum hashing:",filename) 
+    if verbose: log.write(" -md5sum hashing for the file:",filename) 
     md5_hash = hashlib.md5()
     with open(filename,"rb") as f:
         # Read and update hash in chunks
@@ -347,7 +357,4 @@ def md5sum_file(filename,log,verbose):
     with open(filename+".md5sum","w") as f:
         out = str(md5_hash.hexdigest())
         f.write(out+"\n")
-        if verbose: log.write(" -md5sum path:",filename+".md5sum") 
-
-    
-    
+        if verbose: log.write(" -md5sum path:",filename+".md5sum")     
