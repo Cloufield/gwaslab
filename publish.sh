1f72bb957101fbdaf423ff508faf4bb2391c9a4e
Cloufield <610935659@qq.com>
2023-02-03 10:17:22 +0900

    update

--- publish.sh
+++ publish.sh
@@ -1,9 +1,9 @@
 #/bin/bash
-
 git add docs/UpdateLogs.md
 git add docs/index.md
 git add src/gwaslab/version.py
 git add pyproject.toml
-git commit -m "publish"
+git commit -m "publish 3.4.0"
 git push
 python -m build
+python -m twine upload gwaslab-3.4.0*
