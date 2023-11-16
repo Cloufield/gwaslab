9f942eb356bdbae770937c19d80358cdd53ae996
Cloufied <610935659@qq.com>
2023-11-17 00:11:38 +0900

    update

--- docs/Clumping.md
+++ docs/Clumping.md
@@ -1,2 +1,82 @@
 # Clumping in GWASLab by calling PLINK2
 
+GWASLab provides a clumping function using PLINK2. 
+You can get clumping results without worrying about the file format.  
+
+# Clumping 
+
+!!! warning "You need to install PLINK2 first and add to your environment."
+
+```
+clump(  vcf=None, 
+        bfile=None,
+        scaled=False, 
+        out="clumping_plink2", 
+        overwrite=False, 
+        n_cores=2, 
+        chrom=None, 
+        clump_p1=5e-8, 
+        clump_p2=5e-8, 
+        clump_r2=0.2, 
+        clump_kb=250,
+        log=Log())
+```
+
+|Option|DataType|Description|Default|
+|-|-|-|-|
+|`vcf`|`string`|path to reference VCF file (it will be converted to plink binary format). You need to specify either `vcf` or `bfile`|-|
+|`bfile`|`string`|path to PLINK bfile. You need to specify either `vcf` or `bfile`|-|
+|`scaled`|`boolean`|If tru, use MLOG10P instead of P|`False`|
+|`out`|`string`|output file prefix|`clumping_plink2`|
+|`overwrite`|`boolean`|if True, overwrite the existing bfile when vcf path is provided|`False`|
+|`n_cores`|`int`|number of cores to use|2|
+|`clump_p1`|`float`|clump_p1|5e-8|
+|`clump_p2`|`float`|clump_p2|5e-8|
+|`clump_r2`|`float`|clump_r2|0.2|
+|`clump_kb`|`float`|clump_kb|250|
+
+
+!!! info "Plink2 script" 
+    
+    ```
+    # using P
+    plink2 \
+        --bfile {}\
+        --chr {} \
+        --clump {} \
+        --clump-field P \
+        --clump-snp-field SNPID \
+        --clump-p1 {} \
+        --clump-p2 {} \
+        --clump-r2 {} \
+        --clump-kb {} \
+        --threads {} \
+        --out {}
+    ```
+    
+    ```
+    # using MLOG10P
+    plink2 \
+        --bfile {}\
+        --chr {} \
+        --clump {} \
+        --clump-field P \
+        --clump-snp-field SNPID \
+        --clump-p1 {} \
+        --clump-p2 {} \
+        --clump-r2 {} \
+        --clump-kb {} \
+        --threads {} \
+        --out {}
+    ```
+
+# Example 
+
+!!! example
+
+```
+clumps = mysumstats.clump(vcf= "/home/yunye/.gwaslab/EAS.ALL.split_norm_af.1kgp3v5.hg19.vcf.gz")
+
+
+
+```
--- src/gwaslab/clump.py
+++ src/gwaslab/clump.py
@@ -4,7 +4,7 @@ import os
 import pandas as pd
 from gwaslab.Log import Log
 
-def _clump(insumstats, vcf=None, scaled=False, out="clumping_plink2", bfile=None, n_cores=2, 
+def _clump(insumstats, vcf=None, scaled=False, out="clumping_plink2", overwrite=False, bfile=None, n_cores=2, 
           chrom=None, clump_p1=5e-8, clump_p2=5e-8, clump_r2=0.2, clump_kb=250,log=Log()):
     ## process reference
     log.write("Start to perform clumping...")
@@ -27,68 +27,56 @@ def _clump(insumstats, vcf=None, scaled=False, out="clumping_plink2", bfile=None
     log.write(" -Significant variants on CHR: ",list(sumstats["CHR"].unique()))
     
     plink_log=""
+
+    chrlist = sumstats["CHR"].unique()
     
     # process reference file
-    if bfile is None:
-        if vcf is not None:
-            log.write(" -Processing VCF : {}...".format(vcf))
-            for i in sumstats["CHR"].unique():
-                bfile_gwaslab = vcf.replace(".vcf.gz","") + ".{}".format(i)
-                log.write("  -Processing VCF for CHR {}...".format(i))
-                
-                if not os.path.exists(bfile_gwaslab+".bed"):
-                    script_vcf_to_bfile = """
-                    plink2 \
-                        --vcf {} \
-                        --chr {} \
-                        --make-bed \
-                        --rm-dup force-first \
-                        --threads {}\
-                        --out {}
-                    """.format(vcf, i, n_cores, bfile_gwaslab)
-                    
-                    try:
-                        output = subprocess.check_output(script_vcf_to_bfile, stderr=subprocess.STDOUT, shell=True,text=True)
-                        plink_log+=output + "\n"
-                    except subprocess.CalledProcessError as e:
-                        log.write(e.output)
-                        
-                else:
-                    log.write("  -Plink bfile for CHR {} exists. Skipping...".format(i))
-        else:
-            log.write("  -Please provide PLINK bfile or VCF as reference!")
-    else:
-        log.write(" -PLINK bfile as LD reference panel: {}".format(bfile))                  
+    bfile,plink_log = _process_reference(chrlist, bfile, vcf, n_cores,plink_log, log,overwrite)           
     
     ## process sumstats by CHR
     for i in sumstats["CHR"].unique():
         log.write(" -Processing sumstats for CHR {}...".format(i))
-        if scaled == True:
-            sumstats.loc[sumstats["CHR"]==i,["SNPID","MLOG10P"]].to_csv("_gwaslab_tmp.{}.SNPIDP".format(i),index=None,sep="\t")
+        
+        if "@" in bfile:
+            bfile_to_use = bfile.replace("@",str(i))
         else:
-            sumstats.loc[sumstats["CHR"]==i,["SNPID","P"]].to_csv("_gwaslab_tmp.{}.SNPIDP".format(i),index=None,sep="\t")
+            bfile_to_use = bfile
+        
+        # checking # variants
+        try:
+            bim = pd.read_csv(bfile_to_use + ".bim",usecols=[1],header=None,sep="\s+")[1]
+            snplist = sumstats.loc[sumstats["CHR"]==i,"SNPID"]
+            is_on_both = snplist.isin(bim)
+            log.write(" -#variants in reference file: {}...".format(len(bim)))
+            log.write(" -#variants in sumstats: {}...".format(len(snplist)))
+            log.write(" -#variants available in both reference and sumstats: {}...".format(sum(is_on_both)))
+
+            if scaled == True:
+                sumstats.loc[(sumstats["CHR"]==i) &(is_on_both),["SNPID","MLOG10P"]].to_csv("_gwaslab_tmp.{}.SNPIDP".format(i),index=None,sep="\t")
+            else:
+                sumstats.loc[(sumstats["CHR"]==i) & (is_on_both),["SNPID","P"]].to_csv("_gwaslab_tmp.{}.SNPIDP".format(i),index=None,sep="\t")
+        except:
+            log.write(" -Not available for: {}...".format(i))
         
     # create a empty dataframe for combining results from each CHR 
     results = pd.DataFrame()
     
     # clumping using plink
     for i in sumstats["CHR"].unique():
-        if bfile is None:
-            bfile_gwaslab = vcf.replace(".vcf.gz","") + ".{}".format(i)
-
         chrom = i
         # temp file  
         clump = "_gwaslab_tmp.{}.SNPIDP".format(chrom)
         # output prefix
         out_single_chr= out + ".{}".format(chrom)
         
-        if bfile is None:
-            bfile_to_use = bfile_gwaslab
+        if "@" in bfile:
+            bfile_to_use = bfile.replace("@",str(i))
         else:
             bfile_to_use = bfile
-        
+
         log.write(" -Performing clumping for CHR {}...".format(i))
         if scaled == True:
+            # clumping using LOG10P
             script = """
             plink2 \
                 --bfile {}\
@@ -105,6 +93,7 @@ def _clump(insumstats, vcf=None, scaled=False, out="clumping_plink2", bfile=None
                 --out {}
             """.format(bfile_to_use, chrom, clump, clump_log10_p1, clump_log10_p2, clump_r2, clump_kb, n_cores, out_single_chr)    
         else:
+            # clumping using P
             script = """
             plink2 \
                 --bfile {}\
@@ -127,11 +116,61 @@ def _clump(insumstats, vcf=None, scaled=False, out="clumping_plink2", bfile=None
         except subprocess.CalledProcessError as e:
             log.write(e.output)
         #os.system(script)
+        
         clumped = pd.read_csv("{}.clumps".format(out_single_chr),usecols=[2,0,1,3],sep="\s+")
         results = pd.concat([results,clumped],ignore_index=True)
+        
         # remove temp SNPIDP file 
         os.remove(clump)
+    
     results = results.sort_values(by=["#CHROM","POS"]).rename(columns={"#CHROM":"CHR","ID":"SNPID"})
     log.write("Finished clumping.")
     results_sumstats = insumstats.loc[insumstats["SNPID"].isin(results["SNPID"]),:].copy()
     return results_sumstats, plink_log
+
+
+def _process_reference(chrlist, bfile, vcf, n_cores, plink_log, log, overwrite):
+    
+    # return bfile_path and plink_log
+
+    if bfile is None:
+        if vcf is not None:
+            log.write(" -Processing VCF : {}...".format(vcf))
+            
+            for i in chrlist:
+                log.write("  -Processing VCF for CHR {}...".format(i))
+                if "@" in vcf:
+                    vcf_to_load = vcf.replace("@",str(i))
+                else:
+                    vcf_to_load = vcf
+
+                bfile_root = vcf.replace(".vcf.gz","")
+                bfile_prefix = bfile_root + ".{}".format(i)
+                
+                if (not os.path.exists(bfile_prefix+".bed")) or overwrite:
+                    script_vcf_to_bfile = """
+                    plink2 \
+                        --vcf {} \
+                        --chr {} \
+                        --make-bed \
+                        --rm-dup force-first \
+                        --threads {}\
+                        --out {}
+                    """.format(vcf_to_load, i, n_cores, bfile_prefix)
+                    
+                    try:
+                        log.write("  -Converting VCF to bed: {}.bim/bed/fam...".format(bfile_prefix))
+                        output = subprocess.check_output(script_vcf_to_bfile, stderr=subprocess.STDOUT, shell=True,text=True)
+                        plink_log+=output + "\n"
+                    except subprocess.CalledProcessError as e:
+                        log.write(e.output)
+                        
+                else:
+                    log.write("  -Plink bfile for CHR {} exists. Skipping...".format(i))
+                
+            return bfile_root+".@", plink_log
+        else:
+            log.write("  -Please provide PLINK bfile or VCF as reference!")
+    else:
+        log.write(" -PLINK bfile as LD reference panel: {}".format(bfile))      
+        return bfile, plink_log          
