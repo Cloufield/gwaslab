d540cd837933b7bf1ec893ae8ecc5b64d07a2f88
竹内耀平 <takeuchi@takeuchinoMacBook-Air.local>
2024-07-09 13:15:47 +0900

    clean script

--- src/gwaslab/util_abf_finemapping.py
+++ src/gwaslab/util_abf_finemapping.py
@@ -9,9 +9,13 @@ from gwaslab.g_Log import Log
 def calc_abf(insumstats,omega=0.04,log=Log(),verbose=True):
     log.write("Start to calculate approximate Bayesian factor for each {} variant".format(str(len(insumstats))),verbose=verbose)
     se = insumstats["SE"]
+    v = se**2
+    r = omega / (omega+v)
     beta = insumstats["BETA"]
+    z = beta/se
     insumstats = insumstats.copy()
-    insumstats.loc[:, "log_ABF"] = 1/2*np.log(((se*se)/(se*se+omega))) + (omega*beta*beta)/(2*se*se*(se*se+omega))
+    # insumstats.loc[:, "log_ABF"] = 1/2*np.log(((se*se)/(se*se+omega))) + (omega*beta*beta)/(2*se*se*(se*se+omega))
+    insumstats.loc[:, "log_ABF"] = 1/2* (np.log(1-r) + (r * z**2))
     return insumstats
 
 def calc_PIP(insumstats,log=Log(),verbose=True):
