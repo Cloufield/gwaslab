41c263930db106a635f04cacf9a25da0abe3c706
竹内耀平 <takeuchi@takeuchinoMacBook-Air.local>
2024-07-09 12:00:11 +0900

    fix to insumstats

--- src/gwaslab/util_abf_finemapping.py
+++ src/gwaslab/util_abf_finemapping.py
@@ -6,35 +6,35 @@ from gwaslab.g_Log import Log
 # Wakefield, J. A bayesian measure of the probability of false discovery in genetic epidemiology studies. Am J Hum Genet 81, 208–227 (2007).
 
 
-def calc_abf(df,omega=0.04):
-    se = df["SE"]
-    beta = df["BETA"]
-    df = df.copy()
-    df.loc[:, "log_ABF"] = 1/2*np.log(((se*se)/(se*se+omega))) + (omega*beta*beta)/(2*se*se*(se*se+omega))
-    return df
+def calc_abf(insumstats,omega=0.04):
+    se = insumstats["SE"]
+    beta = insumstats["BETA"]
+    insumstats = insumstats.copy()
+    insumstats.loc[:, "log_ABF"] = 1/2*np.log(((se*se)/(se*se+omega))) + (omega*beta*beta)/(2*se*se*(se*se+omega))
+    return insumstats
 
-def calc_PIP(df):
+def calc_PIP(insumstats):
     # Calculate the logarithmic sum of each ABF to find the logarithm of total_abf
-    log_total_abf = np.log(np.sum(np.exp(df["log_ABF"] - np.max(df["log_ABF"])))) + np.max(df["log_ABF"])
-    df = df.copy()
+    log_total_abf = np.log(np.sum(np.exp(insumstats["log_ABF"] - np.max(insumstats["log_ABF"])))) + np.max(insumstats["log_ABF"])
+    insumstats = insumstats.copy()
     # Calculate PIP on a logarithmic scale by subtracting log_total_abf from each log_abf
-    df.loc[:, "log_PIP"] = df['log_ABF'] - log_total_abf
+    insumstats.loc[:, "log_PIP"] = insumstats['log_ABF'] - log_total_abf
     # Convert PIP on logarithmic scale to exponential and back to normal scale
-    df.loc[:, "PIP"] = np.exp(df['log_PIP'])
-    return df
+    insumstats.loc[:, "PIP"] = np.exp(insumstats['log_PIP'])
+    return insumstats
 
-def abf_finemapping(df):
-    df = calc_abf(df)
-    df = calc_PIP(df)
-    return df
+def abf_finemapping(insumstats):
+    insumstats = calc_abf(insumstats)
+    insumstats = calc_PIP(insumstats)
+    return insumstats
 
-def make_cs(df,threshold=0.95):
-    if "PIP" not in list(df.columns):
-        df = abf_finemapping(df)
-    df = df.sort_values(by="PIP",ascending=False)
+def make_cs(insumstats,threshold=0.95):
+    if "PIP" not in list(insumstats.columns):
+        insumstats = abf_finemapping(insumstats)
+    insumstats = insumstats.sort_values(by="PIP",ascending=False)
     pip_sum = 0
     cs = pd.DataFrame()
-    for index, row in df.iterrows():
+    for index, row in insumstats.iterrows():
         cs = pd.concat([cs,pd.DataFrame(row).T])
         pip_sum += row["PIP"]
         if pip_sum > threshold:
