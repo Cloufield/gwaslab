df12d344da6145df109e8aee440aca7c6d2126b8
Cloufied <610935659@qq.com>
2024-12-08 20:07:18 +0900

    update

--- src/gwaslab/io_process_args.py
+++ src/gwaslab/io_process_args.py
@@ -9,14 +9,26 @@ def _extract_kwargs(prefix:str, default:dict, kwargs:dict) -> dict:
     # kwargs: all local kwargs  + args + kwargs
 
     extracted = []
+    extracted_single=dict()
     for key,value in kwargs.items():
+        # kwargs or args
         if key=="kwargs" or key=="args":
             for key_nested,value_nested in kwargs[key].items():
                 if prefix in key_nested and "arg" in key_nested:
-                    extracted.append(value_nested)
+
+                    if len(key_nested.split("_"))<3:
+                        extracted.append(value_nested)
+                    ##
+                    ## prefix_arg_fontsize
+                    else:
+                        print(key_nested.split("_")[-1], value)
+                        extracted_single[key_nested.split("_")[-1]] = value_nested
         else:
+            # local kwargs
             if prefix in key and "arg" in key:
                 extracted.append(value)
+    if len(extracted_single.keys()) >0:
+        extracted.append(extracted_single)
     merged_arg = _merge_and_sync_dic(extracted, default)
     return merged_arg
 
