b9876f5aa7f76621f823739672df45f37aedffe5
Cloufied <610935659@qq.com>
2024-12-21 01:26:48 +0900

    update

---
+++ examples/0_sample_data/download_roadmap.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+i="098"
+
+wget https://egg2.wustl.edu/roadmap/data/byFileType/chromhmmSegmentations/ChmmModels/coreMarks/jointModel/final/E${i}_15_coreMarks_hg38lift_mnemonics.bed.gz
+wget https://egg2.wustl.edu/roadmap/data/byFileType/chromhmmSegmentations/ChmmModels/coreMarks/jointModel/final/E${i}_15_coreMarks_mnemonics.bed.gz
--- examples/chr/make_chr.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-for i in "X"
-#$(seq 1 22)
-do
-gzcat ../t2d_bbj.txt.gz |awk -v i=${i} 'NR==1 || $2==i {print $0}' >t2d_bbj_chr${i}.txt
-done
