6186392c825f2cccd86e5f84f1e21ce6f08b74eb
Yunye <610935659@qq.com>
2024-12-23 12:54:43 +0900

    update

---
+++ update_docs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for i in 1_main_tutorial 2_input_output 3_standardization 4_harmonization 5_internal_utility 6_external_utility 7_visualization 9_reference_management
+do
+
+cp  ./examples/${i}/*.ipynb /home/yunye/work/gwaslab/docs/
+
+done
